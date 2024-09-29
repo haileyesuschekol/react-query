1fb73cae8d20c33c7625f0886386add8eaf1e002
Haileyesus Chekol <teamdevs7@gmail.com>
2024-09-29 22:31:50 +0100

    post request

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useQuery } from "@tanstack/react-query"
+import { useMutation, useQuery } from "@tanstack/react-query"
 
 function App() {
   const { data, error, isLoading } = useQuery({
@@ -9,10 +9,30 @@ function App() {
       ),
   })
 
+  const { mutate } = useMutation({
+    mutationFn: (newPost) =>
+      fetch("https://jsonplaceholder.typicode.com/posts", {
+        method: "POST",
+        body: JSON.stringify(newPost),
+        headers: { "Content-type": "application/json" },
+      }).then((res) => res.json()),
+  })
+
   if (isLoading) return <div>Loading...</div>
   if (error) return <div>Error while fetching</div>
   return (
     <div>
+      <button
+        onClick={() =>
+          mutate({
+            userId: 2000,
+            title: "new Title",
+            body: "this is new body",
+          })
+        }
+      >
+        Post
+      </button>
       {data?.map((post) => {
         return (
           <ul key={post.id}>
