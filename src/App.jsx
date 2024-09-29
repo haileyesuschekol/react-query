053bc851127f9373f02b0037abde693a05d00c67
Haileyesus Chekol <teamdevs7@gmail.com>
2024-09-29 22:04:00 +0100

    fetch and render posts

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
+import { useQuery } from "@tanstack/react-query"
+
 function App() {
+  const { data, error, isLoading } = useQuery({
+    queryKey: ["todo"],
+    queryFn: () =>
+      fetch("https://jsonplaceholder.typicode.com/posts").then((res) =>
+        res.json()
+      ),
+  })
+
+  if (isLoading) return <div>Loading...</div>
+  if (error) return <div>Error while fetching</div>
   return (
-    <>
-      <div>Home</div>
-    </>
+    <div>
+      {data?.map((post) => {
+        return (
+          <ul key={post.id}>
+            <li>Id: {post.id}</li>
+            <li>Title: {post.title}</li>
+            <li>Body: {post.body}</li>
+          </ul>
+        )
+      })}
+    </div>
   )
 }
 
